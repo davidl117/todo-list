af1141423194d2f31f1998dc5f7bfc48b31602c6
davidl117 <103517663+davidl117@users.noreply.github.com>
2022-10-11 15:35:41 -0400

    added more functionality to list, and reuseability for checkbox

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
  const submit = document.getElementById('submit');
  const textBar = document.getElementById('textBar');
  const pText = document.getElementById('pText');
- 
 
 submit.addEventListener('click', function (){
-    const para = document.createElement("p");
-    para.innerText = textBar.value;
+    
     if (textBar.value === ""){
         alert("Put something on the list!"); 
         return;
     };
-    pText.appendChild(para);
-    textBar.value = "";
-    
+
+    const para = document.createElement("p");
+    para.innerText = textBar.value;
+
+    let checkBox = document.createElement('input');
+    checkBox.setAttribute("type","checkbox");
+
     let button = document.createElement('button');
+    button.classList.add('.noShow');
     button.innerHTML = 'Edit';
-    pText.append(button);
 
     let delButton = document.createElement('button');
+    delButton.classList.add('.noShow');
     delButton.innerHTML = 'Delete';
-    pText.append(delButton);
-
-    para.addEventListener('click', function(){
-    para.style.textDecoration = 'line-through';
+        
+    //values that are displayed on submit click
+    pText.appendChild(para);
+    pText.appendChild(button);
+    pText.appendChild(delButton);
+    pText.appendChild(checkBox);
+    textBar.value = "";
+    
+//add/remove line through text when box is clicked
+checkBox.addEventListener('click',() => {
+    if (checkBox.checked == true) {
+      para.style.textDecoration = 'line-through';
+    }
+  else {
+     para.style.textDecoration = 'none';
+  }
 });
-  para.addEventListener('dblclick', () =>{
+
+
+//removes list element
+  delButton.addEventListener('click', () =>{
     pText.removeChild(para);
-  })
+    pText.removeChild(delButton);
+    pText.removeChild(button);
+    pText.removeChild(checkBox);
+  });
 });
 
 
 
-
+// if (checkBox === true){
+//   linethrough
+// }else no line
 
