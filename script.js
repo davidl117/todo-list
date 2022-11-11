1ecb12807d4005095616a5e3c1fa1ec349891553
davidl117 <103517663+davidl117@users.noreply.github.com>
2022-11-11 13:18:50 -0500

    local storage works for initial entreis, need to do same for edit and delete

--- script.js
+++ script.js
@@ -2,33 +2,18 @@
  const textBar = document.getElementById('textBar');
  const pText = document.getElementById('pText');
  const textContainer = document.querySelector('.pText-container');
- var para = textBar.value;
+
  //displaying stored data on load
- var storeTodo = [];
+let storeTodo = [];
 window.addEventListener('load', () => {
   tasks = JSON.parse(localStorage.getItem('toDo'));
   if ( tasks === null){
     storeTodo = [];
   }else {
     storeTodo = tasks;
-    storeTodo.forEach(task => {
-      for (let i = 0; i < storeTodo.length; i++){
-        task[i];
-      }
-      Display();
-    })
+    storeTodo.forEach(Display);
     }
   })
-  // localStorage.setItem('toDo', JSON.stringify(storeTodo));
-  // storeTodo.push(textBar.value);
-  // JSON.parse(localStorage.getItem('toDo'));
-
-//  => {
-//   for (let i = 0; i < storeTodo.length; i++){
-    
-//     Display();
-//   }
-// })
 
 //prevents "form" from submitting 
 submit.addEventListener('click', (e) => {
@@ -40,36 +25,35 @@ submit.addEventListener('click', () => {
         alert("Put something on the list!"); 
         return;
     }   
+
 //local storage setItem
 storeTodo.push(textBar.value);
 localStorage.setItem('toDo', JSON.stringify(storeTodo));
 Display();
 });
 
-
- function Display() {
- 
+ function Display(task) {
 
   //creating elements
   const newDiv = document.createElement('div');
-  newDiv.classList.add('pText-container');
+    newDiv.classList.add('pText-container');
 
   const para = document.createElement("input");
-  para.type = 'text';
-  para.value = textBar.value;
-  para.setAttribute('readonly', true);
-  para.classList.add('pText');
+    para.type = 'text';
+    para.value = textBar.value;
+    para.setAttribute('readonly', true);
+    para.classList.add('pText');
 
   const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
+   checkBox.type = 'checkbox';
 
   const editButton = document.createElement('button');
-  editButton.classList.add('edit');
-  editButton.innerText = "Edit";
+    editButton.classList.add('edit');
+    editButton.innerText = "Edit";
 
   const delButton = document.createElement('button');
-  delButton.classList.add('delete');
-  delButton.innerText = "Delete";
+    delButton.classList.add('delete');
+    delButton.innerText = "Delete";
 
   textBar.value = "";
 
@@ -83,21 +67,17 @@ Display();
   pText.appendChild(editButton);
   pText.appendChild(delButton);
 
-  localStorage.setItem('toDo', JSON.stringify(storeTodo));
-  // if (storeTodo !== null) {
-    storeTodo.forEach(task => {
-      para.value = task
-      JSON.parse(localStorage.getItem('toDo'));
-    }) 
-  // }
+  for (let i = 0; i < storeTodo.length; i++){
+    task[i] = JSON.parse(localStorage.getItem('toDo')); 
+    para.value = task;
+  }
 
-//content uneditable on click away
+    //content uneditable on click away
 editButton.addEventListener('click', () => {
     para.removeAttribute('readonly');
     para.focus();
     para.addEventListener('blur', () => {
       para.setAttribute('readonly', true);
-      // localStorage.setItem('toDo', JSON.stringify(storeTodo));
 
     })
 })
@@ -123,5 +103,5 @@ checkBox.addEventListener('click',() => {
     // newDiv.removeChild(delButton);
     // localStorage.removeItem('toDo');//might need parameter of e
   
-  }) 
- }
+  })
+} 
